941753ca561201bbc207e01582267200e3f38abc
lekha-123 <71594197+lekha-123@users.noreply.github.com>
2021-08-22 16:15:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 var marks = [30,40,45,35];
-
-
+// total and average 
+var total = 0
 function setup() {
   createCanvas(400, 400);
-
+  for(var index=0;index<marks.length;index=index+1){
+    
+total=total+marks[index];
+  }
+  var average=total/marks.length
 }
 
 function draw() {
